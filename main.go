6b3a8772dcdcb7c8ce95e91d5148064f79074867
Kawahara <My.favorite.is.energy@gmail.com>
2026-04-17 14:40:44 +0900

    Display packets to GUI

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 	"github.com/google/gopacket"
 	_ "github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	_"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/widget"
+	_"image/color"
 )
 
 type EtherType []byte
@@ -26,16 +32,35 @@ func (e EtherType) String() string {
 	}
 }
 
-func handlePacket(p gopacket.Packet) {
+type EtherFrame struct {
+	DstMAC net.HardwareAddr
+	SrcMAC net.HardwareAddr
+	Type EtherType
+}
+
+func (e EtherFrame) String() string {
+	return fmt.Sprintf("[%s]: %s -> %s", e.Type, e.SrcMAC, e.DstMAC)
+}
+
+func handlePacket(p gopacket.Packet) EtherFrame {
 	header := p.LinkLayer().LayerContents()
-	etherDstMac, _ := net.ParseMAC(fmt.Sprintf("%X", header[0:6]))
-	etherSrcMac, _ := net.ParseMAC(fmt.Sprintf("%X", header[6:12]))
+	etherDstMAC, _ := net.ParseMAC(fmt.Sprintf("%X", header[0:6]))
+	etherSrcMAC, _ := net.ParseMAC(fmt.Sprintf("%X", header[6:12]))
 	etherType := EtherType(header[12:14])
 
-	fmt.Printf("[%s]: %s -> %s\n", etherType, etherSrcMac, etherDstMac)
+	return EtherFrame{
+		DstMAC: etherDstMAC,
+		SrcMAC: etherSrcMAC,
+		Type: etherType,
+	}
 }
 
 func main() {
+	myApp := app.New()
+	window := myApp.NewWindow("Hello")
+	window.Resize(fyne.NewSize(800, 600))
+
+
 	// eth0
 	handle, err := pcap.OpenLive("en0", 1600, true, pcap.BlockForever)
 	if err != nil {
@@ -46,7 +71,60 @@ func main() {
 	// Decode a packet
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
-	for packet := range packetSource.Packets() {
-		handlePacket(packet) // Do something with each packet.
-	}
+	packets := make(chan EtherFrame, 100)
+
+	var data []EtherFrame
+
+	go func() {
+		for packet := range packetSource.Packets() {
+			packets <- handlePacket(packet)
+		}
+	}()
+
+	table := widget.NewTable(
+		func() (int, int) {
+			return len(data)+1, 3
+		},
+
+		func() fyne.CanvasObject {
+			return widget.NewLabel("")
+		},
+
+		func(id widget.TableCellID, obj fyne.CanvasObject) {
+			label := obj.(*widget.Label)
+
+			if id.Row == 0 {
+				headers := []string{"Type", "Source MAC", "Destination MAC"}
+				label.SetText(headers[id.Col])
+				return
+			}
+
+			p := data[id.Row-1]
+			switch id.Col {
+			case 0:
+				label.SetText(p.Type.String())
+			case 1:
+				label.SetText(p.SrcMAC.String())
+			case 2:
+				label.SetText(p.DstMAC.String())
+			}
+		},
+	)
+	table.SetColumnWidth(0, 80)
+	table.SetColumnWidth(1, 150)
+	table.SetColumnWidth(2, 200)
+
+	go func() {
+		for p := range packets {
+			fyne.Do(func () {
+				if len(data) >= 50 {
+					data = data[1:]
+				}
+				data = append(data, p)
+				table.Refresh()
+			})
+		}
+	}()
+	window.SetContent(table)
+	window.ShowAndRun()
 }
